a2917cac10e61b7ed3b626f97dfe57da54fddfb6
Jeremy Ehrhardt <jeremy@bat-country.us>
2015-08-19 13:44:53 -0700

    Add support for Genderize.io API keys.
    * new DefaultGenderize constructor and GenderizeIoAPI methods for creating clients with API keys
    * simplified DefaultGenderize slightly
    * fixed case where the list of names methods were used to request 0 or 1 name
    * added integration tests
    * updated version to 1.3 (still backwards compatible with 1.2)

--- src/main/java/com/github/irobson/jgenderize/GenderizeIoAPI.java
+++ src/main/java/com/github/irobson/jgenderize/GenderizeIoAPI.java
@@ -10,4 +10,12 @@ public class GenderizeIoAPI {
     public static Genderize create() {
         return new DefaultGenderize();
     }
+
+    /**
+     * @param apiKey Genderize.io API key.
+     * @return A client instance that can make more queries than one without a key.
+     */
+    public static Genderize create(String apiKey) {
+        return new DefaultGenderize(apiKey);
+    }
 }
